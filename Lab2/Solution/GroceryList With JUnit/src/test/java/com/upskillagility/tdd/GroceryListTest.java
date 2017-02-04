88d9dcd27bf69b437448f09923ca72a12d08aaaa
ajitrathore <ajit.rathore@gmail.com>
2017-02-04 13:57:51 -0600

    Add files via upload

---
+++ Lab2/Solution/GroceryList With JUnit/src/test/java/com/upskillagility/tdd/GroceryListTest.java
@@ -0,0 +1,40 @@
+package com.upskillagility.tdd;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ajit on 2/4/17.
+ */
+public class GroceryListTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getListOfItemForShopping() throws Exception {
+
+
+
+    }
+
+    @Test
+    public void setListOfItemForShopping() throws Exception {
+
+    }
+
+    @Test
+    public void dispose() throws Exception {
+
+    }
+
+}
